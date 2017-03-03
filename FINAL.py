9f89aa29bf1d6df6417c6769736ddd450621888b
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-03-03 14:51:43 -0500

    Updated from Brython Server: 3/3/2017 2:51:42 PM

--- FINAL.py
+++ FINAL.py
@@ -1,1 +1,60 @@
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
 
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+black = Color(0, 1)
+thinline = LineStyle(1, black)
+white = Color(0xffffff, 1)
+gray = Color(0x8c8c8c, 1)
+cf = RectangleAsset(20, 30, thinline, gray)
+class MC(Sprite):
+    """
+    Animated space ship
+    """
+    
+
+    def __init__(self, position):
+        super().__init__(cf, position)
+        self.vx = 1
+        self.vy = 1
+        self.vr = 0.01
+        self.thrust = 0
+        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
+        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+        self.fxcenter = self.fycenter = 0.5
+
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+
+    def thrustOn(self, event):
+        self.thrust = 1
+
+    def thrustOff(self, event):
+        self.thrust = 0
+
+
+
+class SpaceGame(App):
+    """
+    Tutorial4 space game example.
+    """
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        black = Color(0, 1)
+        noline = LineStyle(0, black)
+        bg_asset = RectangleAsset(width, height, noline, black)
+        bg = Sprite(bg_asset, (0,0))
+        MC((100,100))
+        MC((150,150))
+        MC((200,50))
+
+    def step(self):
+        for ship in self.getSpritesbyClass(MC):
+            ship.step()
+
+
+myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+myapp.run()
