66db34c78de538e19cbdbfc44de105aec3fbca35
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-03-15 15:50:01 -0400

    Updated from Brython Server: 3/15/2017 3:50:00 PM

--- primes.py
+++ primes.py
@@ -1,1 +1,24 @@
-
+from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
+generation = False
+def gen():
+    global generation
+    generation = not generation
+def spaceKey(event):
+    gen()
+myapp.listenKeyEvent('keydown', 'space', spaceKey)
+a = 1
+primelist=[1]
+while generation == True:
+    b = 0
+    for x in primelist:
+        if a//x = 0:
+            b += 1
+    if b == 0:
+        primelist.append(a)
+        a += 1
+    else:
+        a+=1
+    
+    
+a = sum(primelist)
+myapp.run()
