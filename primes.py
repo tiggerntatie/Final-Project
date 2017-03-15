fce9eff2cadac4372c564b555489b2ea990e4ddc
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-03-15 16:08:37 -0400

    Updated from Brython Server: 3/15/2017 4:08:36 PM

--- primes.py
+++ primes.py
@@ -1,5 +1,34 @@
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
-myapp=App()
+SCREEN_WIDTH = 740
+SCREEN_HEIGHT = 570
+class Conway(App):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        black = Color(0, 1)
+        white = Color(0xffffff, 1)
+# Background
+
+        noline = LineStyle(0, black)
+        
+        bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, white)
+        bg = Sprite(bg_asset, (0,0))
+        
+#Step Function
+    def step(self):
+        if generation == True:
+            print("Yay")
+            b = 0
+            for x in primelist:
+                print(x)
+                if int(a)%int(x) != 0:
+                    b += 1
+                    print(b)
+            if b == 0:
+                primelist.append(a)
+                a += 1
+            else:              
+                a+=1
+myapp=Conway(SCREEN_WIDTH, SCREEN_HEIGHT)
 generation = False
 def gen():
     global generation
@@ -7,21 +36,12 @@ def gen():
 def spaceKey(event):
     gen()
     print(primelist)
+    print(generation)
 myapp.listenKeyEvent('keydown', 'space', spaceKey)
 a = 2
 primelist=[1]
-while generation == True:
-    b = 0
-    for x in primelist:
-        print(x)
-        if int(a)%int(x) == 0:
-            b += 1
-    if b == 0:
-        primelist.append(a)
-        a += 1
-    else:
-        a+=1
+
     
     
-a = sum(primelist)
+
 myapp.run()
