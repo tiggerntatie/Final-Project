3fb3956ea95d1168f67184e1ad9aab931bf929c6
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-03-15 17:26:23 -0400

    Updated from Brython Server: 3/15/2017 5:26:22 PM

--- primes.py
+++ primes.py
@@ -1,39 +1,17 @@
-from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame
-SCREEN_WIDTH = 740
-SCREEN_HEIGHT = 570
-myapp=App()
-generation = False
-
-primelist=[1]
-
+MAX = int(input("How many primes should I count? "))
+i = 0
 a = 2
-
-
-   
-def spaceKey(event):
-    MAX = int(input("How many primes should I count? "))
-    i = 0
-    global a
+primelist = [1]
     
-    while i !=MAX:
-        b = 0
-        for x in primelist:
-            if int(a)%int(x) == 0:
-                b += 1
-                
-        if b == 1:
-            primelist.append(a)
-            a += 1
-            
-            i+=1
-        else:              
-            a+=1
-            
-    print(primelist)
-myapp.listenKeyEvent('keydown', 'space', spaceKey)
-
-
-    
-    
-
-myapp.run()
+while i !=MAX:
+    b = 0
+    for x in primelist:
+        if int(a)%int(x) == 0:
+            b += 1
+    if b == 1:
+        primelist.append(a)
+        a += 1
+        i+=1
+    else:              
+        a+=1
+print(primelist)
