70003b5fdcbf8e2be8001de33dab0717476b80d7
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-03-29 13:48:38 -0400

    Updated from Brython Server: 3/29/2017 1:48:37 PM

--- FINAL.py
+++ FINAL.py
@@ -17,7 +17,7 @@ white = Color(0xffffff, 1)
 gray = Color(0x8c8c8c, 1)
 noline = LineStyle(0, black)
 thinline1 = LineStyle(1, white)
-flashlight= PolygonAsset([(-640, 480), (640, 480), (640, -480), (10, -480), (10, 0), (30, -60), (-10, -60), (10,0), (10,-480), (640, 480)], noline, black)
+flashlight= PolygonAsset([(-640, 480), (-640, -480), (640, -480),(640, 480) (10, 480), (10, 0), (30, -60)], noline, black)
 Leftside = Sprite(flashlight, (320, 240))
 
 cf = RectangleAsset(20, 30, thinline, gray)
