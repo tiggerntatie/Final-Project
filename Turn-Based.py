c4ed09f49d996b3eb9dfedb5777396143f89e7a7
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-04-14 14:29:11 -0400

    Updated from Brython Server: 4/14/2017 2:29:11 PM

--- Turn-Based.py
+++ Turn-Based.py
@@ -1,6 +1,7 @@
 from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Frame, PolygonAsset
 from math import pi, cos, sin, atan2, sqrt
 SCREEN_WIDTH1 = 640
+hit = 0
 SCREEN_HEIGHT = 480
 black = Color(0, 1)
 speed = 10
@@ -9,7 +10,7 @@ red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 swordlist = []
-
+time = 0
 thinline = LineStyle(1, black)
 white = Color(0xffffff, 1)
 gray = Color(0x8c8c8c, 1)
@@ -19,7 +20,7 @@ cf = PolygonAsset(((-10,-15),(10,-15),(10,15),(-10,15)), thinline, gray)
 ms = PolygonAsset(((-7.5,-11.5),(7.5,-11.5),(7.5,11.5),(-7.5,11.5)), thinline, red)
 smlSword = PolygonAsset(((-2.5,-5),(2.5,-5),(2.5,5),(-2.5,5)), thinline, red)
 class MC(Sprite):
-    def __init__(self, position):
+    def __init__(self, position, lives):
         super().__init__(cf, position)
         self.moves = speed1
         SpaceGame.listenKeyEvent("keydown", "d", self.dKey)
@@ -61,7 +62,6 @@ class MC(Sprite):
         else:
             self.rotation-= pi/2
     def step(self):
-        
         self.moves = speed1
 class meleeSprite(Sprite):
     def __init__(self, position): 
@@ -71,16 +71,18 @@ class meleeSprite(Sprite):
         self.rotation = pi/2-atan2((self.y-MC1.y), (self.x-MC1.x))
         self.x -= 10*cos(atan2(self.y-MC1.y, self.x-MC1.x))
         self.y -= 10*sin(atan2(self.y-MC1.y, self.x-MC1.x))
-        
-        if MC1.rotation == 0 or MC1.rotation == pi:
-            if sqrt((self.x-MC1.x)**2+(self.y-MC1.y)**2) <= 20:
-                print("yay")
-                swordlist.append(sword(((self.x-15*cos(atan2(self.y-MC1.y, self.x-MC1.x))), (self.y-15*sin(atan2(self.y-MC1.y, self.x-MC1.x)))), self.rotation))
+        if sqrt((self.x-MC1.x)**2+(self.y-MC1.y)**2) <= 20:
+            swordlist.append(sword(((self.x-15*cos(atan2(self.y-MC1.y, self.x-MC1.x))), (self.y-15*sin(atan2(self.y-MC1.y, self.x-MC1.x)))), self.rotation))
+            global hit
+            hit +=1
 class sword(Sprite):
     def __init__(self, position, rotation): 
         super().__init__(smlSword, position)
         self.fxcenter = self.fycenter = 0.5
         self.rotation = rotation
+    def step(self):
+        self.x = self.y = 10000
+        
         
     
     
@@ -93,17 +95,30 @@ class SpaceGame(App):
         black = Color(0, 1)
         noline = LineStyle(0, black)
         bg_asset = RectangleAsset(width, height, noline, white)
+        dead_asset = RectangleAsset(width, height, noline, red)
+        
         bg = Sprite(bg_asset, (0,0))
         
     def step(self):
         if turn == 1:
             for x in swordlist:
-                x.destroy
-            
+                x.step()
+            swordlist = []
+            print(swordlist)
             meleeSprite1.step()
             MC1.step()
             global turn
             turn = 0
+        if hit > 0:
+            time +=1
+            if time > 5:
+                hit = 0
+                time = 0
+                dead.destroy()
+            dead = Sprite(dead_asset, (0,0))
+            
+            
+            
         
             
 
@@ -116,6 +131,9 @@ def turnProgress ():
     turn=1
 def spaceKey (event):
     turnProgress()
+    
+    
+    
     
 myapp.listenKeyEvent('keydown', 'space', spaceKey)
 MC1=MC((320,240))
