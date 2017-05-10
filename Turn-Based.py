46d241a8c2dfd6b4c091071a53bd0c3e032cf5f2
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-05-10 13:25:06 -0400

    Updated from Brython Server: 5/10/2017 1:25:06 PM

--- Turn-Based.py
+++ Turn-Based.py
@@ -381,6 +381,13 @@ while GG != 1:
     else:
         x+=38
         lp+=1
+while GG != 0:
+    ammolist[lp]=ammo((myapp.width-15-x,myapp.height-y*3), lp)
+    if lp == lives-1:
+        GG = 0
+    else:
+        x+=38
+        lp+=1
     
     
 turn = 0
