c875acaacd5ffd52c8980498e66cbc78dfbc2e74
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-05-12 14:42:29 -0400

    Updated from Brython Server: 5/12/2017 2:42:29 PM

--- Turn-Based.py
+++ Turn-Based.py
@@ -394,7 +394,7 @@ class spdSprite(Sprite):
             else:
                 self.rotation +=pi/60
     def attack(self):
-        if time.time()-self.start<self.jumpTime
+        if time.time()-self.start<self.jumpTime:
             self.x -= self.jumpTime*cos(-self.rotation+pi/2)
             self.y -= self.jumpTime*sin(-self.rotation+pi/2)
             if len(myapp.collidingwithSprites(MC))>0:
