8e68b567e8f5c9da6ab241ccfdc905c939ddb5a1
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-06-08 16:55:54 -0400

    Updated from Brython Server: 6/8/2017 4:55:51 PM

--- timer.py
+++ timer.py
@@ -1,1 +1,27 @@
 from ggame import Sprite, TextAsset
+import time
+time=60
+def timer():
+    time=time
+    currentTime= time.time()
+    timeList=[]
+    def timer2():
+        if time.time()-currentTime()>1:
+            time-=1
+            timeAsset=TextAsset(time)
+            print(time, timeList)
+            if len(timeList)>0:
+                for x in timeList:
+                    x.destroy
+            timeList=[]
+            timeList.append(Sprite(timeAsset, (200, 20)))
+            print(timeList)
+            timer()
+        elif:
+            timer2()
+timer()
+            
+            
+            
+    
+    
