e08efc63d696d6b280a3e92a2547f86b33937aa6
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-06-09 12:24:43 -0400

    Updated from Brython Server: 6/9/2017 12:24:42 PM

--- timer.py
+++ timer.py
@@ -3,17 +3,23 @@ import time
 timeCounter=60
 cT= time.time()
 timeList=[]
+timeReset = False
 def timer():
-    global cT, timeCounter
+    print('hi')
+    global cT, timeCounter, timeList, timeReset
     if timeReset==True:
+        print('hi')
         cT=time.time
-        timeReset==False
+        timeReset=False
     elif time.time()-cT>1:
         if len(timeList)>0:
             for x in timeList:
                 x.destroy()
         timeAsset= TextAsset(timeCounter)
-        timeList.append(Sprite(timeAsset, (100,100))
+        timeList.append(Sprite(timeAsset, (100,100)))
+        timeReset = True
+        print(timeList)
+timer()
         
             
     
