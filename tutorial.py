8ab4824c7cc79151fda0a911773e610fb5fc674e
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-06-13 12:47:09 -0400

    Create tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -11,13 +11,13 @@ def tutorial1():
         slideNumber=2
 def tutorial2():
     global error
-    nd = input('{0}Q and E rotate you. J is a short ranged melee attack that kills stuff. K is a bullet. You have 4 shots before you have to use R to reload. Press the "r" key if you understand'.format(error))
+    nd = input('{0}Q and E rotate you. Z is a boost. J is a short ranged melee attack that kills stuff. K is a bullet. You have 4 shots before you have to use R to reload. Press the "r" key if you understand'.format(error))
     if nd != 'r':
         error = "ERROR, TRY AGAIN"
         tutorial2()
 def tutorial3():
     global error
-    nd = input('{0}L is a shield with a turn two cooldown. Speaking of turns, you have a limited number of moves 4. Rotating is free. Moving is one move, as is melee attacking. Shooting and reloading is 3 moves, while shielding is 2. Press Space to get your moves back and have the enemies attack. Press the "space" key if you understand'.format(error))
+    nd = input('{0}L is a shield with a turn two cooldown. Speaking of turns, you have a limited number of moves 4. Rotating is free. Moving is one move, as is melee attacking. Boosting is 2. Shooting and reloading is 3 moves, while shielding is 2. Press Space to get your moves back and have the enemies attack. Press the "space" key if you understand'.format(error))
     if nd != ' ':
         error = "ERROR, TRY AGAIN"
         tutorial3()
@@ -35,6 +35,7 @@ controlAsset= 0
 def controls():
     print("""CONTROLS:
         WASD TO MOVE (1 move per)
+        z: boost (2 move per)
         J: short range attack (1 move per)
         K: long range attack (3 move per)
         SPACE: end turn
@@ -44,6 +45,7 @@ def controls():
     global controlAsset
     controlAsset= """CONTROLS:
         WASD TO MOVE (1 move per)
+        z: boost (2 move per)
         J: short range attack (1 move per)
         K: long range attack (3 move per)
         SPACE: end turn
